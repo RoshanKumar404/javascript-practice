8f44c9c14f600aae1369ec49b3757eb4e53ff2ea
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-21 13:29:26 +0530

    learned slice and filter and indexof

---
+++ practice1-2.js
@@ -0,0 +1,8 @@
+const fileExtension=(str)=>str.slice(str.lastIndexOf( '.'));
+console.log(fileExtension('rohan.cpp'));
+
+const filenames=['new.html','bro.js','dude.jsx','hell.cpp','hi.py'];
+const log= filenames.map(names=> names.substring(names.lastIndexOf('.')))
+console.log(log);
+const blog=log.filter(str=>str==='.cpp')
+console.log(blog);
