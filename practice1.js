247495a2fe33657a8e580a8140cd6770b7153ab0
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-21 10:33:58 +0530

    revising javascript

---
+++ practice1.js
@@ -0,0 +1,19 @@
+// var a = 100;
+
+// const b= 19;
+// const c = 81;
+// if (b+c===100){
+//     console.log(true)
+// }
+// else{
+//     console.log(false);
+// }
+const equality=(a,b)=>a===100||b===100||(a+b)===100;
+console.log(equality(24,34));
+console.log(equality(100,34));
+console.log(equality(24,100));
+console.log(equality(100,100));
+console.log(equality(24,34));
+console.log(equality(50,50));
+
+
